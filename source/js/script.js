353a1dae78ae66c4dfa50ba062ed4f37eeb5bd82
firetrik116 <vasinokezhka@gmai.com>
2022-10-19 23:16:14 +0300

    init

--- source/js/script.js
+++ source/js/script.js
@@ -3,11 +3,32 @@ const modal = document.querySelector(".user-nav__list");
 const toggle = document.querySelector(".user-nav__line--center");
 const wire = document.querySelector(".user-nav__line--top");
 const down = document.querySelector(".user-nav__line--down");
+let select_input = document.querySelector(".select-menu__input");
+let select_menu = document.querySelector(".select-menu");
+let select_items = document.querySelectorAll(".select-menu__item");
+let slider = document.getElementById("sliderRange");
+let output = document.getElementById("demo");
 
 
 
 
+select_input.addEventListener("click",()=>{
+  select_menu.classList.toggle("active");
 
+
+});
+
+select_items.forEach((item) =>{
+ item.addEventListener("click", (evt)=>{
+  select_input.textContent = evt.target.textContent
+  select_menu.classList.toggle("active");
+ }) 
+}); 
+
+output.innerHTML = slider.value;
+slider.oninput = function(){
+  output.innerHTML = this.value;
+}
 
   link.addEventListener("click", function (evt) {
     evt.preventDefault();

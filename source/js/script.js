57fd13efc16c12c8eb4bef97d47c869750062c4c
firetrik116 <vasinokezhka@gmai.com>
2022-10-21 10:51:49 +0300

    fix

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,5 @@
-const link = document.querySelector(".user-nav__burger");
+const burger = document.querySelector(".user-nav__burger");
 const modal = document.querySelector(".user-nav__list");
-const toggle = document.querySelector(".user-nav__line--center");
-const wire = document.querySelector(".user-nav__line--top");
-const down = document.querySelector(".user-nav__line--down");
 let select_input = document.querySelector(".select-menu__input");
 let select_menu = document.querySelector(".select-menu");
 let select_items = document.querySelectorAll(".select-menu__item");
@@ -13,7 +10,7 @@ let output = document.getElementById("demo");
 
 
 select_input.addEventListener("click",()=>{
-  select_menu.classList.toggle("active");
+  select_menu.classList.toggle("select-menu--active");
 
 
 });
@@ -21,36 +18,20 @@ select_input.addEventListener("click",()=>{
 select_items.forEach((item) =>{
  item.addEventListener("click", (evt)=>{
   select_input.textContent = evt.target.textContent
-  select_menu.classList.toggle("active");
+  select_menu.classList.toggle("select-menu--active");
  }) 
 }); 
 
-output.innerHTML = slider.value;
+
+
 slider.oninput = function(){
-  output.innerHTML = this.value;
+  output.innerHTML = `${this.value}%`;
 }
 
-  link.addEventListener("click", function (evt) {
+  burger.addEventListener("click", function (evt) {
     evt.preventDefault();
+    burger.classList.toggle("user-nav__burger--active");
     modal.classList.toggle("user-nav__list--active");
   });
-  
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    toggle.classList.toggle("active--center");
-  });
 
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    wire.classList.toggle("active--wire");
-  });
 
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    down.classList.toggle("active--down");
-  });
-
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    link.classList.toggle("user-nav__burger--active");
-  });
